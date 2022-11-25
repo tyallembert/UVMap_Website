ff9724a055144a79ba672ae52ab02786077c2d1f
Nate Lalor <natelalor@gmail.com>
2022-11-25 00:41:31 -0500

    leftSide descriptions of each page. will style later.

--- script.js
+++ script.js
@@ -3,27 +3,27 @@ class Tour {
 		this.pages = {
 			1: {
 				"title": "Signup Page",
-				"description": "signup description",
+				"description": "The signup page is where it all begins. Requiring a uvm.edu email address, we provide backend protection to ensure that only students from the University of Vermont are able to use the features UVMaps provides. With a simple username and password, you can make the UVM journey of your dreams come true!",
 				"image": "images/signup.png"
 			},
 			2: {
 				"title": "Login Page",
-				"description": "Login description",
+				"description": "While the login page not be the most exciting at a first glance, it is working hard behind the scenes. Requiring a username and password, the login page works with our backend to keep your account credentials secure and efficiently accessible. You will be on your journey before you can even say, \"Wow UVMaps! You are simply amazing! I should email the developers and let them know how awesome they are!\"",
 				"image": "images/login.gif"
 			},
 			3: {
 				"title": "Map Page",
-				"description": "map description",
+				"description": "Now that you are safe and sound, logged in to the app, you reach the map page. The map page is where it all goes down - choosing your destination, looking at the possible stops along the way, and more. If you fancy a bike ride, UVMaps got you covered! We provide routes for both walking and biking methods of travel. While you are on your journey, we also provide you an ETA to help with your daily time management as much as we can. You gotta study, and we will get you there.",
 				"image": "images/map.gif"
 			},
 			4: {
 				"title": "Schedule Page",
-				"description": "Schedule description",
+				"description": "We know as students you have a lot on your plate. While we've got you covered for travel assistance, we wanted to do more. Our schedule page includes advanced UI that provides a weekly presentation of the classes you have to alleviate your stressful daily schedules. As well as showing your schedule, there is also the snazzy function of adding classes. Pulling directly from UVM's class lists, you will be able to search through our database and select the classes you are taking. The 'when and where' is what UVMaps was designed for.",
 				"image": "images/schedule.gif"
 			},
 			5: {
 				"title": "Settings Page",
-				"description": "Settings description",
+				"description": "The settings page allows users to tweak UVMaps app exactly to their liking. Prefer apps in dark mode? You got it. Want an altered ETA? Sure thing. Here is where you can unlock the full potential to UVM travel. You will never lose to your classmates when racing to class. Thanks, UVMaps!",
 				"image": "images/settings.png"
 			}
 		}
